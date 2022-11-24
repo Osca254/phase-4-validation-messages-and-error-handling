bbd5ce264f592f59cf3979caf070e7705ed8f978
moringa <oscarokumu47@gmail.com>
2022-11-24 16:45:47 +0300

    Complited Assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,9 +6,31 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def show
+    movies = Movie.find_by(id: params[:id])
+    if movies
+      render json: movies,status: :ok
+    else
+      render json: {error: "Movie not found"},status: :not_found
+    end
+
+  end
+
+  def destroy
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      movie.destroy
+    else
+      render json: {error: "Movie not found"},status: :not_found
+    end
+  end
+
   def create
     movie = Movie.create(movie_params)
     render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+  render json: {errors:
+e.record.errors.full_messages }, status: :unprocessable_entity
   end
 
   private
